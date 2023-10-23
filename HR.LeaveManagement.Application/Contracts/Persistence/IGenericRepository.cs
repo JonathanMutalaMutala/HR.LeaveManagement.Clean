336bdabfa9db5ae73432feaa4f190ef27ce2cb3d
Mutala Mutala <jonamut12.live@gmail.com>
2023-10-23 00:01:18 -0400

    Ajout de la couche d'application et les interfaces des entitées

---
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,45 @@
+﻿namespace HR.LeaveManagement.Application.Contracts.Persistence
+{
+    /// <summary>
+    /// Interface public qui Accepte T ou Type est une classe 
+    /// </summary>
+    /// <typeparam name="T">Represente un Type </typeparam>
+    public interface IGenericRepository<T> where T : class
+    {
+       
+        /// <summary>
+        /// Permet de récupérer toutes les entités
+        /// </summary>
+        /// <returns></returns>
+        Task<T> GetAsync();
+
+        /// <summary>
+        /// Permet de récupérer une entité par son Id
+        /// </summary>
+        /// <param name="id">Représente l'ID de l'entité</param>
+        /// <returns></returns>
+        Task<T> GetByIdAsync(int id);
+        /// <summary>
+        ///¨Créer un entité 
+        /// </summary>
+        /// <param name="entity">Represente L'objet de l'entité à créer</param>
+        /// <returns></returns>
+        Task<T> CreateAsync(T entity);
+
+        /// <summary>
+        /// Permet de mettre à jour une entité 
+        /// </summary>
+        /// <param name="entity">Represente l'objet de l'entité à mettre à jour</param>
+        /// <returns></returns>
+        Task<T> UpdateAsync(T entity);
+
+        /// <summary>
+        /// Supprimer une entité 
+        /// </summary>
+        /// <param name="entity">Represente l'objet à supprimer</param>
+        /// <returns></returns>
+        Task<T> DeleteAsync(T entity);
+       
+    }
+
+}
