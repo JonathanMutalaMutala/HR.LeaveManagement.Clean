e7166738d6c7b57d9ee831a11ef557f21b51030e
Mutala Mutala <jonamut12.live@gmail.com>
2023-10-23 00:34:31 -0400

    Ajout de Automapper et MediatR

---
+++ HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.MappingProfiles
+{
+
+    public class LeaveTypeProfile : Profile
+    {
+        public LeaveTypeProfile()
+        {
+            CreateMap<LeaveTypeDto, LeaveTypeProfile>().ReverseMap();
+        }
+    }
+}
