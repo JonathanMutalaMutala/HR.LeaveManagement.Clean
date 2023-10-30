9fef496a2c1aaef126bd4637a18d8958dd5722b6
Mutala Mutala <jonamut12.live@gmail.com>
2023-10-30 02:26:04 -0400

    Mise en place de LeaveTypeDetails Handler dans le pattern CQRS

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypeDetailsQueryHandler.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails
+{
+    /// <summary>
+    /// Cette classe provient du pattern CQRS Command Query Responsability Segregation 
+    /// herite de la IRequestHandler permet de handle les Query envoyer et de renvoyer le Dto qu'il faut 
+    /// 
+    /// </summary>
+    public class GetLeaveTypeDetailsQueryHandler : IRequestHandler<GetLeaveTypeDetailsQuery, LeaveTypeDetailsDto>
+    {
+        /// <summary>
+        /// Permet de mapper les Data 
+        /// </summary>
+        private readonly IMapper _autoMapper;
+
+        /// <summary>
+        /// Permet de recuperer la methode concerner au type de methode à  retourner 
+        /// </summary>
+        private readonly ILeaveTypeRepository _leaveTypeRepository; 
+
+
+        public GetLeaveTypeDetailsQueryHandler(IMapper autoMapper,ILeaveTypeRepository leaveTypeRepository) 
+        {
+            _autoMapper = autoMapper;
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+
+        /// <summary>
+        /// Cette methode permet de recuperer le Details d'un LeaveType, de le convertir en Dto et de retourner 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<LeaveTypeDetailsDto> Handle(GetLeaveTypeDetailsQuery request, CancellationToken cancellationToken)
+        {
+            var leaveType = await _leaveTypeRepository.GetByIdAsync(request.Id);
+
+            var data = _autoMapper.Map<LeaveTypeDetailsDto>(leaveType);
+
+            return data;
+        }
+    }
+}
--- HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
+++ HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
 using HR.LeaveManagement.Domain;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,8 @@ namespace HR.LeaveManagement.Application.MappingProfiles
         {
             //Je peux faire les mappages dans le deux sens de mon Dto à mon Entity ou de mon Entity à mon Dto
             CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDetailsDto>(); // Je ne reverse pas puisque je vais aller de LeaveType et convertir en LeaveTypeDetailsDto
+
         }
     }
 }
