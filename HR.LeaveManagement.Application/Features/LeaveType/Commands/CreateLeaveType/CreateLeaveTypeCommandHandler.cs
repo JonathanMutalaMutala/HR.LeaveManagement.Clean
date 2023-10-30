589d45dae384f484747fad0545da8c2d206c253e
Mutala Mutala <jonamut12.live@gmail.com>
2023-10-30 02:41:07 -0400

    Mise en place de createLeavCommand utilisant le pattern CQRS

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType
+{
+    public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateleaveTypeCommand, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public CreateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            this._mapper = mapper;
+            this._leaveTypeRepository = leaveTypeRepository;
+        }
+
+        public  async Task<int> Handle(CreateleaveTypeCommand request, CancellationToken cancellationToken)
+        {
+
+
+            //Convert to domain entity object 
+            var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
+
+
+            await _leaveTypeRepository.CreateAsync(leaveTypeToCreate);
+
+
+            return leaveTypeToCreate.Id;
+
+        }
+    }
+}
