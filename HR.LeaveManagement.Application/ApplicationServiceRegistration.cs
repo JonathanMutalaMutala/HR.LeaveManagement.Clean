8b3f551358ca36da12b112dc73189abfbaf00cc9
Mutala Mutala <jonamut12.live@gmail.com>
2023-10-30 01:24:22 -0400

    .

--- HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
+++ HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -9,11 +9,22 @@ using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application
 {
+    /// <summary>
+    /// Classe permettant de faire l'injection de dépendance dans l'application 
+    /// </summary>
     public static class ApplicationServiceRegistration
     {
+        /// <summary>
+        /// Méthode d'extension qui prend un objet IserviceCollection 
+        /// </summary>
+        /// <param name="services">Service qu'on souhaite injecter </param>
+        /// <returns></returns>
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
+            //Ajout du service autoMapper à la collection des services 
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            // Ajout du services MediatR dans la collection de services 
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 
             return services;
