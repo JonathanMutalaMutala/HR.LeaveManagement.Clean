e97ec9962526ef82cef71aed457a9240a702f72a
Mutala Mutala <jonamut12.live@gmail.com>
2023-11-19 02:49:28 -0500

    Mise en place du DbContext pour Identity

---
+++ HR.LeaveManagement.Identity/IdentityServicesRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Identity
+{
+    public static class IdentityServicesRegistration
+    {
+        public static IServiceCollection ConfigureIdentityServices(this  IServiceCollection services,IConfiguration configuration)
+        {
+
+        }
+    }
+}
