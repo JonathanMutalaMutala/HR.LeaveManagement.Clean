13af655f9c97b420e1b17176ccf364f49544a241
Mutala Mutala <jonamut12.live@gmail.com>
2023-11-21 15:16:17 -0500

    Fix Employee return null try to get a LeaveRequestDetails

--- HR.LeaveManagement.Application/Features/LeaveRequest/Query/GetLeaveRequestDetail/GetLeaveRequestDetailQueryHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Query/GetLeaveRequestDetail/GetLeaveRequestDetailQueryHandler.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
 using HR.LeaveManagement.Application.Features.LeaveRequest.Query.GetLeaveRequestList;
+using HR.LeaveManagement.Application.Identity;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -13,18 +15,28 @@ namespace HR.LeaveManagement.Application.Features.LeaveRequest.Query.GetLeaveReq
     public class GetLeaveRequestDetailQueryHandler :IRequestHandler<GetLeaveRequestDetailQuery,LeaveRequestDetailsDto>
     {
         public readonly ILeaveRequestRepository _leaveRequestRepository;
-        public readonly IMapper _mapper; 
+        public readonly IMapper _mapper;
+        public readonly IUserService _userService;
 
-        public GetLeaveRequestDetailQueryHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        public GetLeaveRequestDetailQueryHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper, IUserService userService)
         {
             _leaveRequestRepository = leaveRequestRepository;
             _mapper = mapper;
+            _userService = userService;
         }
 
         public async Task<LeaveRequestDetailsDto> Handle(GetLeaveRequestDetailQuery request, CancellationToken cancellationToken)
         { 
             var leaveRequest = _mapper.Map<LeaveRequestDetailsDto>(await _leaveRequestRepository.GetLeaveRequestWithDetails(request.Id));
 
+            if(leaveRequest == null)
+            {
+                throw new NotFoundException(nameof(LeaveRequest), request.Id);
+            }
+
+            //Add employee details 
+
+            leaveRequest.Employee = await _userService.GetEmployeeByIdAsync(_userService.UserId);  
 
             return leaveRequest;
         }
